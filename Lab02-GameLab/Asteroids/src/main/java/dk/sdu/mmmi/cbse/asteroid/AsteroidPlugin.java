700686898b77f8cef61856f629c1a6ca24b7ee82
Berkanktk <berkank@live.dk>
2022-03-16 14:41:56 +0100

    Added asteroid, player and enemy to lab02

---
+++ Lab02-GameLab/Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
@@ -0,0 +1,48 @@
+package dk.sdu.mmmi.cbse.asteroid;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+import java.util.Random;
+
+public class AsteroidPlugin implements IGamePluginService {
+
+    private Entity asteroid;
+    private Random random = new Random();
+
+    @Override
+    public void start(GameData gameData, World world) {
+        asteroid = createAsteroid(gameData);
+        world.addEntity(asteroid);
+    }
+
+    private Entity createAsteroid(GameData gameData) {
+
+        float deceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 100;
+        float rotationSpeed = 5;
+
+        // Create random start position
+        int x = random.nextInt(gameData.getDisplayWidth());
+        int y = random.nextInt(gameData.getDisplayHeight());
+        float radians = 3.1415f * random.nextFloat();
+
+        // Add new asteroid entity with defined properties
+        Entity asteroid = new Asteroid();
+        asteroid.setRadius(20);
+        asteroid.add(new MovingPart(deceleration, acceleration, maxSpeed, rotationSpeed));
+        asteroid.add(new PositionPart(x, y, radians));
+
+        return asteroid;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeEntity(asteroid);
+    }
+}
---
+++ Lab02-GameLab/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -0,0 +1,56 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.util.Random;
+
+
+public class EnemyPlugin implements IGamePluginService {
+
+    private Entity enemy;
+    private Random random = new Random();
+
+    public EnemyPlugin() {}
+
+    @Override
+    public void start(GameData gameData, World world) {
+
+        // Add entities to the world
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+    }
+
+    private Entity createEnemyShip(GameData gameData) {
+
+        //Create random start position
+        int randomX = random.nextInt(gameData.getDisplayWidth());
+        int randomY = random.nextInt(gameData.getDisplayHeight());
+        float randomRadians = 3.1415f * random.nextFloat();
+
+        //Set movement rules
+        float deacceleration = 10;
+        float acceleration = 200;
+        float maxSpeed = 300;
+        float rotationSpeed = 5;
+
+        //Set enemy ship start position
+        float x = randomX;
+        float y = randomY;
+        float radians = randomRadians;
+
+        Entity enemyShip = new Enemy();
+        enemyShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        enemyShip.add(new PositionPart(x, y, radians));
+
+        return enemyShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        world.removeEntity(enemy);
+    }
+}
